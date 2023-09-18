bbedabec18ec3695389fcf1150ce81dd5ae082f6
Niasbel <asbelkipkosgei861@gmail.com>
2023-09-18 09:57:39 -0700

    Size of integers

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print the size of bits
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("Size of a char: %lu bits\n", sizeof(char));
+	printf("size of an int: %lu bits\n", sizeof(int));
+	printf("Size of a float: %lu bits\n", sizeof(float));
+	return (0);
+}
