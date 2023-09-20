047dfd68cd59b5461176ae532f5eb3a1f8bcb8e5
Niasbel <asbelkipkosgei861@gmail.com>
2023-09-20 06:34:25 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the dig numbers of base 10
+ * starting from 0 followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
