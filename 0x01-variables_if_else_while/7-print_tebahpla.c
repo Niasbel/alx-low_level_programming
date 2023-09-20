4dfad77e9bb14a6ec4349d08e2ee92e9bd6b03ad
Niasbel <asbelkipkosgei861@gmail.com>
2023-09-20 07:37:28 -0700

    Alphabets in reverse mode

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabets in reverse mode
+ * Return: 0.
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 'z'; a <= 'a'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
