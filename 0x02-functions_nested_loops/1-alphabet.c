474c5a84aab280f9da0789da320dabe90d5863ef
Niasbel <asbelkipkosgei861@gmail.com>
2023-09-22 05:49:52 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: always 0.
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+		_putchar('\n');
+	}
+	return (0);
+}
